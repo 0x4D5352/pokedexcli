9698333b863fb34d68e9e0172548799ba07331a3
Joshua Nussbaum <67082011+0x4D5352@users.noreply.github.com>
2024-09-18 18:09:56 -0500

    add cache init

---
+++ internal/pokecache/pokecache.go
@@ -0,0 +1,20 @@
+package pokecache
+
+import (
+	"sync"
+	"time"
+)
+
+type Cache struct {
+	entries map[string]cacheEntry
+	mu      sync.Mutex
+}
+
+type cacheEntry struct {
+	createdAt time.Time
+	val       []byte
+}
+
+func NewCache(interval time.Duration) {
+
+}
