e40b4d4a6090171c3407384c66d0f89a86435b0c
Joshua Nussbaum <67082011+0x4D5352@users.noreply.github.com>
2024-09-19 14:20:49 -0500

    start adding cache

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -15,6 +15,23 @@ type cacheEntry struct {
 	val       []byte
 }
 
+type timed interface {
+	Add() error
+	Get() ([]byte, bool) // NOTE: should this have an error? or is the bool the error state (found/not found)?
+	reapLoop() error
+}
+
 func NewCache(interval time.Duration) {
 
+}
+
+func (c *Cache) Add(key string, val []byte) error {
+	return nil
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	return []byte{}, false
+}
+func (c *Cache) reapLoop() error {
+	return nil
 }
